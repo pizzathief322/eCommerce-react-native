562223df6c5e37f43541e6e4f15921d8730c65eb
pizzathief322 <120076870+pizzathief322@users.noreply.github.com>
2022-12-08 12:16:23 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MyCart from './src/screens/MyCart';
+import ProductInfo from './src/screens/ProductInfo';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MyCart" component={MyCart} />
+        <Stack.Screen name="ProductInfo" component={ProductInfo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
